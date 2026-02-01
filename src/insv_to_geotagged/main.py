43476ac2d8daaffc79d8e37e22fed1d9eb5fa04a
Daniel <danielbailie@gmail.com>
2026-02-01 11:25:21 +0000

    added support for track point heading and added a number of gps meta data tags.

--- src/insv_to_geotagged/main.py
+++ src/insv_to_geotagged/main.py
@@ -6,7 +6,7 @@ import gpxpy
 from pathlib import Path
 from datetime import datetime, timedelta,timezone
 import av
-import exiftool
+
 
 def extract_gps(insv_path : Path, gpx_path : Path) -> int:
     if gpx_path.exists():
@@ -31,6 +31,7 @@ def extract_gps(insv_path : Path, gpx_path : Path) -> int:
     
     return result.returncode
 
+
 def get_start_time(insv_path : Path) -> datetime:
     cmd = [
         "exiftool",
@@ -54,57 +55,8 @@ def get_start_time(insv_path : Path) -> datetime:
         start_time = start_time.replace(tzinfo=timezone.utc)
     return start_time
 
-# def get_duration(insv_path : Path) -> timedelta:
-#     cmd = [
-#             "ffprobe", "-v", "error",
-#             "-select_streams", "v:0",
-#             "-show_entries", "stream=duration",
-#             "-of", "csv=p=0",
-#             insv_path
-#         ]
-#     out = subprocess.check_output(cmd, text=True).strip()
-#     return timedelta(seconds=float(out)) if out else None
 
-# def get_frames_and_pts(path):
-#     cmd = [
-#         "ffprobe",
-#         "-v", "error",
-#         "-select_streams", "v:0",
-#         "-show_packets",
-#         "-show_entries", "packet=pts_time",
-#         "-of", "csv=p=0",
-#         path
-#     ]
-
-#     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-#     for frame_number, line in enumerate(proc.stdout):
-#         ts = line.strip()
-#         if ts:
-#             yield frame_number, timedelta(seconds=float(ts))
-
-#     proc.wait()
-
-
-
-# def lerp_points(point_a : gpxpy.mod_gpx.GPXTrackPoint, point_b : gpxpy.mod_gpx.GPXTrackPoint, time:datetime) -> gpxpy.mod_gpx.GPXTrackPoint:
-#     time_a = point_a.time
-#     time_b = point_b.time
-#     diff = time_b - time_a
-#     delta = time - time_a
-#     f = delta / diff
-
-#     return gpxpy.mod_gpx.GPXTrackPoint(
-
-#         time=time
-#     )
-
-#     print(f"a:{time_a}    b:{time_b}     t:{time}     f:{f}")
-#     return point_a
-#                     # TODO: If between two points -- approx position!
-#                     # return mod_geo.Location(point.latitude, point.longitude)
-
-def get_est_position_at_time(gpx: gpxpy.mod_gpx.GPX, time:datetime):
+def get_est_position_at_time(gpx: gpxpy.mod_gpx.GPX, time:datetime) -> list[gpxpy.mod_gpx.GPXTrackPoint]:
     points = []
     for track in gpx.tracks: 
         for segments in track.segments:
@@ -126,33 +78,6 @@ def is_any_point_closer(points:list[gpxpy.mod_gpx.GPXTrackPoint], target:gpxpy.m
         if point.distance_2d(target) < distance:
             return True
     return False
-
-
-# def gen_desired_frame_indexes(insv_path: Path, start_time: datetime, gpx:gpxpy.mod_gpx.GPX, min_distance:float):
-#     selected_points = []
-#     selected_frames = []
-
-#     discarded_frames_count = 0
-
-#     for frame, pts in get_frames_and_pts(insv_path):
-#         frame_time = start_time + pts
-
-#         found_points = get_est_position_at_time(gpx, frame_time)
-#         if len(found_points) > 0:
-#             pos = found_points[0]
-
-#             if is_any_point_closer(selected_points,pos,min_distance):
-#                 discarded_frames_count +=1
-#             else:
-#                 selected_points.append(pos)
-#                 selected_frames.append({
-#                     "frame": frame,
-#                     "frame_time": frame_time,
-#                     "position": pos,
-#                 })
-    
-#     return selected_frames,discarded_frames_count
-
 
 
 def v360_yaw_filter_graph(width: int, height: int, pix_fmt: str, time_base, rotation_deg: float):
@@ -202,19 +127,6 @@ def extract_frames(path:Path,out_path:Path, start_time:datetime, gpx:gpxpy.mod_g
     cc.thread_type = "AUTO"   # or "FRAME" / "SLICE" depending on codec
     cc.thread_count = 0       # 0 = FFmpeg chooses (often == CPU cores)
 
-    # # Output stream
-    # out = av.open(out_path / "frames_%06d.jpg", mode="w", format="image2")
-    # output_stream = out.add_stream("png")           # FFmpeg's JPEG encoder
-    # output_stream.width = input_stream.width
-    # output_stream.height = input_stream.height
-    # output_stream.pix_fmt = "rgb24"                # common JPEG pix fmt
-    # output_stream.options = {"compression_level": "8"}              # lower = better quality (2–5 typical)
-
-    # # Enable multithreaded encoding
-    # cc = output_stream.codec_context
-    # cc.thread_type = "AUTO"   # or "FRAME" / "SLICE" depending on codec
-    # cc.thread_count = 0       # 0 = FFmpeg chooses (often == CPU cores)
-
     filter_graph = None
     src = None
     sink = None
@@ -231,7 +143,7 @@ def extract_frames(path:Path,out_path:Path, start_time:datetime, gpx:gpxpy.mod_g
             if len(found_points) > 1:
                 print (f"Warning: multiple gps positions were found for frame time {frame_time}, only first will be used")
             position = found_points[0]
-        
+
             if not is_any_point_closer(extracted_points,position,min_distance):
                 #extract frame
 
@@ -246,8 +158,6 @@ def extract_frames(path:Path,out_path:Path, start_time:datetime, gpx:gpxpy.mod_g
 
                 src.push(frame)            
                 filtered_frame = sink.pull()
-                #for pkt in output_stream.encode(filtered_frame):
-                #    out.mux(pkt)
 
                 file_name = out_path / f"frames_{len(extracted_frames):0>5}.jpg"
                 filtered_frame.to_image().save(file_name, quality=90, optimize=False)
@@ -269,14 +179,10 @@ def extract_frames(path:Path,out_path:Path, start_time:datetime, gpx:gpxpy.mod_g
             frame.time / float(elapsed_time.seconds)
         ),end="\r")
 
-    # flush output stream
-    # for pkt in output_stream.encode():
-    #     out.mux(pkt)
-    # out.close()
-
     print("")
     print(f"frame extraction complete frames: {len(extracted_frames)} discarded: {index - len(extracted_frames)}")
     return extracted_frames
+
 
 def offset_to_str(dt):
     off = dt.utcoffset()
@@ -292,20 +198,27 @@ def offset_to_str(dt):
 
     return f"{sign}{hours:02d}:{minutes:02d}"
 
+
 def write_metadata(extracted_frames:list, out_path: Path):
     print("writing metadata csv")
     csv_path = out_path/"metadata.csv"
     csv_file = open(csv_path,"w")
     csv_file.write(
         "SourceFile,"
+        #time
         "AllDates,"
         "SubSecTimeOriginal,"
         "SubSecTimeDigitized,"
         "OffsetTime,"
         "OffsetTimeOriginal,"
         "OffsetTimeDigitized,"
+        #gps
         "GPSLatitude,"
         "GPSLongitude,"
+        "GPSLatitudeRef,"
+        "GPSLongitudeRef,"
+        "GPSImgDirection,"
+        #constants
         "-XMP-GPano:ProjectionType,"
         "Make,"
         "Model,"
@@ -318,14 +231,20 @@ def write_metadata(extracted_frames:list, out_path: Path):
         time_zone_offset = offset_to_str(extracted_frame["frame_time"])
         csv_file.write(
             f"{extracted_frame["file_path"]},"
+            #time
             f"{time_stamp_whole},"
             f"{time_stamp_ms:03d},"
             f"{time_stamp_ms:03d},"
             f"{time_zone_offset},"
             f"{time_zone_offset},"
             f"{time_zone_offset},"
+            #gps
             f"{extracted_frame["position"].latitude},"
             f"{extracted_frame["position"].longitude},"
+            "N,"
+            "E,"
+            f"{extracted_frame["position"].course},"
+            #constants
             "equirectangular,"
             "Insta360,"
             "X4,"
@@ -346,6 +265,20 @@ def write_metadata(extracted_frames:list, out_path: Path):
     ]
     result = subprocess.run(cmd, stdout=sys.stdout, stderr=sys.stderr)
 
+
+def load_gpx(path : Path) -> gpxpy.mod_gpx.GPX:
+    gpx_file = open(path, 'r')
+    gpx = gpxpy.parse(gpx_file)
+
+    #load extension fields
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+
+                for extension in point.extensions:
+                    if extension.tag == 'heading':
+                        point.course = float(extension.text)
+    return gpx
 
 
 @click.command()
@@ -371,8 +304,7 @@ def main(path,yaw,min_distance,out_path):
         return 2
 
     #load gpx file
-    gpx_file = open(gpx_path, 'r')
-    gpx = gpxpy.parse(gpx_file)
+    gpx = load_gpx(gpx_path)
 
     #get start time
     start_time = get_start_time(path)
@@ -384,20 +316,5 @@ def main(path,yaw,min_distance,out_path):
     extracted_frames = extract_frames(mp4_path,out_path,start_time,gpx,min_distance,yaw)
 
     write_metadata(extracted_frames, out_path)
-    # #get duration
-    # duration = get_duration(path)
-    # if duration == None:
-    #     print("Error: Failed to extract duration from input file")
-    #     return 4
-    # print(f"Duration: {duration}")
 
-    # #end time
-    # end_time = start_time + duration
-    # print(f"End Time: {end_time}")
-
-    # desired_frames,discarded_count = gen_desired_frame_indexes(path,start_time,gpx,min_distance)
-
-    # print(f"Desired: {len(desired_frames)}, discarded: {discarded_count}")
-
-   
     return 0
